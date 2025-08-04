ef2c6b3be8212270a3a3f3c054e221bff48d5fb4
Dilani <dilanithathsarani2003@gmail.com>
2025-08-04 13:43:26 +0530

    forms created & changed

--- src/pages/admin/addProductForm.jsx
+++ src/pages/admin/addProductForm.jsx
@@ -1,24 +1,149 @@
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { Link, useNavigate } from "react-router-dom";
+import mediaUpload from "../../utills/mediaUpload";
+
 
 export default function AddProductForm() {
-return(
-    <div className="w-full h-screen rounded-lg flex justify-center items-center">
-        <div className="w-[500px] h-[600px] rounded-lg shadow-lg bg-white flex flex-col items-center">
-            <h1>Add Product</h1>
-            
-            <input className="w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]" placeholder="product Id" />
-            <input className="w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]" placeholder="product Name" />
-            <input className="w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]" placeholder="Alternative Names" />
-            <input className="w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]" placeholder="Price" />
-            <input className="w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]" placeholder="Labeled Price" />
-            <textarea className="w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]" placeholder="Description" />
-            <input className="w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]" placeholder="Stock" />
+	const [productId, setProductId] = useState("");
+	const [name, setName] = useState("");
+	const [altNames, setAltNames] = useState("");
+	const [price, setPrice] = useState("");
+	const [labeledPrice, setLabeledPrice] = useState("");
+	const [description, setDescription] = useState("");
+	const [stock, setStock] = useState("");
+	const [images, setImages] = useState([]);
+	const navigate = useNavigate();
 
-            <div className="w-[400px] h-[100px] flex justify-between items-center">
-                <Link to={"/admin/products"} className="bg-red-600 text-white p-[10px] w-[180px] text-center rounded-lg hover:bg-red-600">Cancel</Link>
-                <button className="bg-green-500 cursor-pointer text-white p-[10px] ml-[10px] w-[180px] text-center rounded-lg hover:bg-green-600">Add Product</button>
-            </div>
-        </div>
-    </div>
-)
+	async function handleSubmit() {
+		const promisesArray = [];
+		for (let i = 0; i < images.length; i++) {
+			const promise = mediaUpload(images[i])
+			promisesArray[i] = promise;
+		}
+		try {
+			const result = await Promise.all(promisesArray);
+
+			const altNamesInArray = altNames.split(",");
+			const product = {
+				productId: productId,
+				name: name,
+				altNames: altNamesInArray,
+				price: price,
+				labeledPrice: labeledPrice,
+				description: description,
+				stock: stock,
+				images: result,
+			};
+			const token = localStorage.getItem("token");
+			console.log(token);
+
+			await axios.post(import.meta.env.VITE_BACKEND_URL + "/api/product", product, {
+					headers: {
+						Authorization: "Bearer " + token,
+					},
+				})
+			toast.success("Product added successfully");
+			navigate("/admin/products");
+				
+		} catch (error) {
+			console.log(error);
+			toast.error("Product adding failed");
+		}
+	}
+
+	return (
+		<div className="w-full h-full rounded-lg  flex justify-center items-center">
+			<div className="w-[500px] h-[600px]  rounded-lg shadow-lg flex flex-col items-center">
+				<h1 className="text-3xl font-bold text-gray-700 m-[10px]">
+					Add Product
+				</h1>
+				<input
+					value={productId}
+					onChange={(e) => {
+						setProductId(e.target.value);
+					}}
+					className="w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]"
+					placeholder="product ID"
+				/>
+				<input
+					value={name}
+					onChange={(e) => {
+						setName(e.target.value);
+					}}
+					className="w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]"
+					placeholder="Product Name"
+				/>
+				<input
+					value={altNames}
+					onChange={(e) => {
+						setAltNames(e.target.value);
+					}}
+					className="w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]"
+					placeholder="Alternative Names"
+				/>
+				<input
+					value={price}
+					onChange={(e) => {
+						setPrice(e.target.value);
+					}}
+					type="number"
+					className="w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]"
+					placeholder="Price"
+				/>
+				<input
+					value={labeledPrice}
+					onChange={(e) => {
+						setLabeledPrice(e.target.value);
+					}}
+					type="number"
+					className="w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]"
+					placeholder="Labelled Price"
+				/>
+				<textarea
+					value={description}
+					onChange={(e) => {
+						setDescription(e.target.value);
+					}}
+					className="w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]"
+					placeholder="Description"
+				/>
+				<input
+					type="file"
+					onChange={(e) => {
+						setImages(e.target.files);
+					}}
+					multiple
+					className="w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]"
+					placeholder="Product Images"
+				/>
+
+				{/* stock */}
+				<input
+					value={stock}
+					onChange={(e) => {
+						setStock(e.target.value);
+					}}
+					type="number"
+					className="w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]"
+					placeholder="Stock"
+				/>
+				<div className="w-[400px]  h-[100px]  flex justify-between items-center rounded-lg">
+					<Link
+						to={"/admin/products"}
+						className="bg-red-500 text-white p-[10px] w-[180px] text-center rounded-lg  hover:bg-red-600 "
+					>
+						Cancel
+					</Link>
+					<button
+						onClick={handleSubmit}
+						className="bg-green-500 cursor-pointer text-white p-[10px] w-[180px] text-center rounded-lg ml-[10px] hover:bg-green-600"
+					>
+						Add Product
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
---
+++ src/utills/mediaUpload.jsx
@@ -0,0 +1,36 @@
+import {createClient} from "@supabase/supabase-js";
+
+const supabase = createClient(
+    "https://ahcpbpiqnejztmingajl.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFoY3BicGlxbmVqenRtaW5nYWpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQyODkxNDksImV4cCI6MjA2OTg2NTE0OX0.LOfXMTyg_2APCZdQGG5q6exZ1TgXVE05yjL3OeQ8AJk"
+);
+
+export default function mediaUpload(file){
+    const promise=new Promise(
+        (resolve,reject)=>{
+             if(file == null){
+                reject("No file selected")
+            }
+            const timeStamp = new Date().getTime()
+            const newFileName = timeStamp+file.name
+
+            supabase.storage.from("images").upload(newFileName, file, {
+                cacheControl: "3600",
+                upsert: false,
+            }).then(
+                ()=>{
+                    const url = supabase.storage.from("images").getPublicUrl(newFileName).data.publicUrl
+                    resolve(url)
+                }
+            ).catch(
+                (error)=>{
+                    console.log(error)
+                    reject("File upload failed")
+                }
+            )
+        }
+    )
+
+    return promise
+
+        }
