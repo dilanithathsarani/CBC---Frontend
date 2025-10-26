d4bb50d41b6b2b30aaa68f0f3f589252c146e876
dilanithathsarani <dilanithathsarani2003@gmail.com>
2025-10-27 01:38:35 +0530

    cart

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "../components/header";
 import ProductsPage from "./client/productsPage";
+import CartPage from "./client/cart";
 import Footer from "../components/footer";
 import ProductOverview from "./client/productOverview";
 
@@ -9,11 +10,12 @@ export default function HomePage(){
     return(
         <div className="w-full h-screen">
             <Header/>
-            <div className="w-full min-h-[calc(100vh-70px)]">
+            <div className="w-full h-[calc(100vh-70px)] min-h-[calc(100vh-70px)]">
                 <Routes path="/*">
                     <Route path="/" element={<h1>Home Page</h1>}/>
                     <Route path="/products" element={<ProductsPage/>}/>
                     <Route path="/overview/:id" element={<ProductOverview/>}/>
+                    <Route path="/cart" element={<CartPage/>}/>
                     <Route path="/*" element={<h1>404 Not Found</h1>} />
                 </Routes>
             </div>
