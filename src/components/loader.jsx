99d6280bbb92eef3a83570a36cabccb881acb88f
dilanithathsarani <dilanithathsarani2003@gmail.com>
2025-12-07 19:26:13 +0530

    loader colour updated

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -1,7 +1,7 @@
-export default function Loader(){
-    return(
-        <div className="w-full h-full  flex justify-center items-center">
-			<div className="w-[70px] h-[70px] border-[4px] border-transparent border-t-[#86003C] rounded-full animate-spin"></div>
-		</div>
-    )
+export default function Loader() {
+  return (
+    <div className="w-full h-full flex justify-center items-center bg-transparent">
+      <div className="w-[70px] h-[70px] rounded-full border-4 border-t-[#86003C] border-b-[#F75270] border-l-[#F7CAC9] border-r-[#FDEBD0] animate-spin"></div>
+    </div>
+  );
 }
